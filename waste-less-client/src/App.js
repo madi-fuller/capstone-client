b3842c6df2b17869edc335fcbf3f7802beba2299
Madi Fuller <madi.fuller595@gmail.com>
2024-03-19 19:21:25 -0400

    make folders for all parts of project

--- waste-less-client/src/App.js
+++ waste-less-client/src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from './pages/HomePage/HomePage';
+
 
 function App() {
   return (
+    <div className = "app">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />}/>
+      </Routes>
+    </BrowserRouter>
     <div>
+    </div>
     </div>
   );
 }
