b5854b8f42aa1e8602ec7e9bf2d6c3f0fa8acc02
Madi Fuller <madi.fuller595@gmail.com>
2024-03-20 10:00:54 -0400

    initial styling for header

--- waste-less-client/src/components /Header/Header.js
+++ waste-less-client/src/components /Header/Header.js
@@ -3,43 +3,33 @@ import "./Header.scss";
 
 function Header() {
   return (
-    <div>
+    <div className="header">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
         <div class="container-fluid">
           <img
             className="header__logo"
             src={logo}
             alt="WasteLess logo with globe"
           />
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
+          <div class="collapse navbar-collapse header__navigation" id="navbarNav">
             <ul class="navbar-nav">
-              <li class="nav-item">
+              <li class="nav-item header__nav-item">
                 <a class="nav-link active" aria-current="page" href="/">
                   Home
                 </a>
               </li>
-              <li class="nav-item">
+              <li class="nav-item header__nav-item">
                 <a class="nav-link" href="/">
                   Waste Log
                 </a>
               </li>
-              <li class="nav-item">
+              <li class="nav-item header__nav-item">
                 <a class="nav-link" href="/">
                   Recipes
                 </a>
               </li>
-              <li class="nav-item">
+              <li class="nav-item header__nav-item">
                 <a class="nav-link" href="/">
                   Profile
                 </a>
