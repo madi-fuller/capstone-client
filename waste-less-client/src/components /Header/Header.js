5f0f91e17b06b29bbf008bca9425429e15b7ba1d
Madi Fuller <madi.fuller595@gmail.com>
2024-03-19 22:58:09 -0400

    create header navigation bar

--- waste-less-client/src/components /Header/Header.js
+++ waste-less-client/src/components /Header/Header.js
@@ -1,14 +1,55 @@
 import logo from "../../assets/logos/wasteless-logo.png";
+import "./Header.scss";
 
-function Header () {
-    return (
-        <div>
-            <nav>
-                
-            </nav>
-            
+function Header() {
+  return (
+    <div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <img
+            className="header__logo"
+            src={logo}
+            alt="WasteLess logo with globe"
+          />
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/">
+                  Waste Log
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/">
+                  Recipes
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="/">
+                  Profile
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
-    )
+      </nav>
+    </div>
+  );
 }
 
 export default Header;
