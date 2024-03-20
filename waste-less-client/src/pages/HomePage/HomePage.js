1568130310502625a15b25ea8717e7a2ec8bd98a
Madi Fuller <madi.fuller595@gmail.com>
2024-03-19 20:30:01 -0400

    add logo and subtitle

--- waste-less-client/src/pages/HomePage/HomePage.js
+++ waste-less-client/src/pages/HomePage/HomePage.js
@@ -1,7 +1,17 @@
+import logo from "../../assets/logos/logo-no-image.png";
+import welcome from "../../assets/images/homepage-planet.png";
+import "./HomePage.scss";
+
 function HomePage() {
     return (
-        <div>
-            <p> This is home page</p>
+        <div className="homepage">
+            <div className="homepage__logo-container">
+            <img className="homepage__logo" src={logo} alt="WasteLess logo" />
+            </div>
+            <div className="homepage__logo-container">
+            <h2 className="homepage__subheading"> Make a Commitment to Waste Less Food in Your Home</h2>
+            </div>
+            <img className="homepage__image" src={welcome} alt="globe saying hi"/>
         </div>
     )
 }
