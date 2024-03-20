82fef2b341849f14d8c11261ddf169a142273e91
Madi Fuller <madi.fuller595@gmail.com>
2024-03-19 21:29:57 -0400

    initial styling for homepage

--- waste-less-client/src/components /PledgeForm/PledgeForm.js
+++ waste-less-client/src/components /PledgeForm/PledgeForm.js
@@ -1,26 +1,28 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./PledgeForm.scss";
 
 function PledgeForm() {
   return (
-    <div>
-      <form>
+    <div className="pledge-form">
+        <h3 className="pledge-form__title"> Take the Pledge!</h3>
+      <form >
         <div className="mb-3">
           <label htmlFor="name" className="form-label">
-            Name
+            Name:
           </label>
           <input
             type="text"
-            className="form-control"
+            className="pledge-form__input form-control "
             id="name"
             placeholder="Enter your name"
           />
         </div>
         <div className="mb-3">
           <label htmlFor="reason" className="form-label">
-            Reason for reducing waste
+            Reason for reducing waste:
           </label>
           <textarea
-            className="form-control"
+            className="form-control pledge-form__input"
             id="reason"
             rows="3"
             placeholder="Enter your reason"
@@ -30,14 +32,15 @@ function PledgeForm() {
           <label for="datePicker" class="form-label">
             Select Date:
           </label>
-          <input type="date" class="form-control" id="datePicker" />
+          <input type="date" class="form-control pledge-form__input" id="datePicker" />
         </div>
-        <button type="submit" className="btn btn-primary">
-          Submit
+        <button type="submit" className="btn btn-primary pledge-form__button">
+          Commit to Change
         </button>
       </form>
-    </div>
-  );
+      </div>
+  )
 }
+
 
 export default PledgeForm;
