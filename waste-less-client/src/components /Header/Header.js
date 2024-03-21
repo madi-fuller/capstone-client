d03f1813479007826e0a3b84f0c0aaa314826a0b
Madi Fuller <madi.fuller595@gmail.com>
2024-03-21 17:05:07 -0400

    style nav links

--- waste-less-client/src/components /Header/Header.js
+++ waste-less-client/src/components /Header/Header.js
@@ -1,37 +1,42 @@
-import logo from "../../assets/logos/wasteless-logo.png";
 import "./Header.scss";
+import logo from "../../assets/logos/wasteless-logo.png";
+
 import { NavLink } from "react-router-dom";
 
 function Header() {
   return (
     <div className="header">
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-
         <div class="container-fluid">
-          <img
-            className="header__logo"
-            src={logo}
-            alt="WasteLess logo with globe"
-          />
-          <div class="collapse navbar-collapse header__navigation" id="navbarNav">
+          <NavLink to="/">
+            <img
+              className="header__logo"
+              src={logo}
+              alt="WasteLess logo with globe"
+            />
+          </NavLink>
+          <div
+            class="collapse navbar-collapse header__navigation"
+            id="navbarNav"
+          >
             <ul class="navbar-nav">
-              <li class="nav-item header__nav-item">
-                <NavLink to="/" class="nav-link active" aria-current="page">
+              <li class="nav-item">
+                <NavLink to="/" class=" header__nav-item nav-link active" aria-current="page">
                   Home
                 </NavLink>
               </li>
-              <li class="nav-item header__nav-item">
-                <NavLink class="nav-link" to="/waste-log">
+              <li class="nav-item">
+                <NavLink class="header__nav-item nav-link" to="/waste-log">
                   Waste Log
-             </NavLink>
+                </NavLink>
               </li>
-              <li class="nav-item header__nav-item">
-                <NavLink class="nav-link" to="/">
+              <li class="nav-item">
+                <NavLink class="header__nav-item nav-link" to="/">
                   Recipes
                 </NavLink>
               </li>
-              <li class="nav-item header__nav-item">
-                <NavLink class="nav-link" to="/">
+              <li class="nav-item">
+                <NavLink class="header__nav-item nav-link" to="/">
                   Profile
                 </NavLink>
               </li>
