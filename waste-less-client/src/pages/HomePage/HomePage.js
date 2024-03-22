c587ecd7bdd735ab218d780e24da940aaa462d4b
Madi Fuller <madi.fuller595@gmail.com>
2024-03-21 20:06:37 -0400

    open the modal when the commit button in hero is clicked

--- waste-less-client/src/pages/HomePage/HomePage.js
+++ waste-less-client/src/pages/HomePage/HomePage.js
@@ -1,33 +1,33 @@
 import welcome from "../../assets/images/homepage-planet.png";
 import "./HomePage.scss";
-
 import PledgeForm from "../../components /PledgeFormModal/PledgeForm";
+import { useState } from "react";
 
 function HomePage() {
+  const [modalOpen, setModalOpen] = useState(false);
   return (
     <div>
       <div className="homepage">
-      <PledgeForm/>
+        {modalOpen && <PledgeForm />}
         <div className="homepage__hero-container">
-        <div className="homepage__hero-container-text">
-          <h2 className="homepage__hero-title">Join the Fight Against Food Waste</h2>
-          <h4 className="homepage__hero-text">
-            Empower yourself to make a difference in the fight against food
-            waste. Discover practical solutions and sustainable habits that are
-            beneficial for both you and the planet.
-          </h4>
-          <button className="homepage__hero-button">Commit to Change</button>
+          <div className="homepage__hero-container-text">
+            <h2 className="homepage__hero-title">
+              Join the Fight Against Food Waste
+            </h2>
+            <h4 className="homepage__hero-text">
+              Empower yourself to make a difference in the fight against food
+              waste. Discover practical solutions and sustainable habits that
+              are beneficial for both you and the planet.
+            </h4>
+            <button className="homepage__hero-button" onClick={() => setModalOpen(true)}>Commit to Change</button>
           </div>
           <img
             className="homepage__image"
             src={welcome}
             alt="globe saying hi"
           />
-          
         </div>
-        <div className="homepage__pledge-container">
-            
-        </div>
+        <div className="homepage__pledge-container"></div>
       </div>
     </div>
   );
