1f1bb3b8e21bb050881d473753c84a68aab99802
Madi Fuller <madi.fuller595@gmail.com>
2024-03-24 18:29:54 -0400

    add delete functionality when x icon is clicked

--- waste-less-client/src/components /WasteItemsList/WasteItemsList.js
+++ waste-less-client/src/components /WasteItemsList/WasteItemsList.js
@@ -1,11 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import remove from "../../assets/icons/remove.png";
+import "./WasteItemsList.scss";
 
 function WasteItemsList() {
   const [wasteItem, setWasteItem] = useState([]);
+
   useEffect(() => {
     const API_URL = "http://localhost:8080/api/add-waste";
-
     const getWasteItems = async () => {
       try {
         const response = await axios.get(API_URL);
@@ -15,7 +17,18 @@ function WasteItemsList() {
       }
     };
     getWasteItems();
-  }, [wasteItem]);
+  }, []);
+
+  const deleteItem = async (itemId) => {
+    const API_URL = `http://localhost:8080/api/add-waste/${itemId}`;
+    try {
+      await axios.delete(API_URL);
+      setWasteItem(prevItems => prevItems.filter(item => item.id !== itemId));
+    } catch(error) {
+      console.error("An error ocurred while deleting the item", error);
+    }
+  }
+
   return (
     <section className="waste-log">
       <div className="container">
@@ -27,6 +40,8 @@ function WasteItemsList() {
                         <h5 className="card-title"> {item.name}</h5>
                         <p className="card-text">{item.category}</p>
                         <p className="card-text">{item.quantity}</p>
+                        <img onClick={() => deleteItem(item.id)} className="waste-log__icon card-text " src={remove} alt="x icon" />
+
                         </div>
                     </div>
             </div>
