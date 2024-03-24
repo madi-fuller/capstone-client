11d407ff83bf059eba735acb1102b8884d366ac2
Madi Fuller <madi.fuller595@gmail.com>
2024-03-24 17:27:12 -0400

    add styling for waste page

--- waste-less-client/src/pages/WasteLog/WasteLog.js
+++ waste-less-client/src/pages/WasteLog/WasteLog.js
@@ -2,21 +2,26 @@
 import WasteItemsList from "../../components /WasteItemsList/WasteItemsList";
 import AddWasteItem from "../../components /AddWasteItem/AddWasteItem";
 import { useState } from "react";
+import "./WasteLog.scss";
 
 function WasteLog() {
   const [modalOpen, setModalOpen] = useState(false);
   return (
-    <>
+    <section className="waste-page">
       <div className="waste-header">
+        <div className="waste-header__container">
         <h1 className="waste-header__title">Wasted Food</h1>
         <p className="waste-header__subtitle">
           Keep track of the food items you throw away here
         </p>
+        </div>
+        <div className="waste-header__container">
         <button className="waste-header__button" onClick={() => setModalOpen(true)}>Add item</button>
+      </div>
       </div>
       <WasteItemsList />
       {modalOpen && <AddWasteItem />}
-    </>
+    </section>
   );
 }
 
