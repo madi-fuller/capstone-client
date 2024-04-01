8936a8d7a0ac888934f794dd2333daa5ba107a47
Madi Fuller <madi.fuller595@gmail.com>
2024-03-31 22:48:43 -0400

    randomize the tips with a button click

--- waste-less-client/src/components /WasteTips/WasteTips.js
+++ waste-less-client/src/components /WasteTips/WasteTips.js
@@ -3,44 +3,51 @@ import axios from "axios";
 import lightbulb from "../../assets/icons/lightbulb.png";
 import { useEffect, useState } from "react";
 
-function WasteTips () {
+function WasteTips() {
+  const [wasteTips, setWasteTips] = useState([]);
 
-    const [wasteTips, setWasteTips] = useState([]);
+  
+    const getTips = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:8080/api/waste-tips/"
+        );
+        setWasteTips(response.data);
+      } catch (error) {
+        console.error("Error getting tips", error);
+      }
+    };
 
     useEffect(() => {
-        const getTips = async () => {
-            try {
-                const response = await axios.get("http://localhost:8080/api/waste-tips/");
-                setWasteTips(response.data);
-            } catch(error) {
-                console.error("Error getting tips", error);
-            }
-        }
-        getTips();
-    })
+    getTips();
 
-    return (
-        <div className="tips">
-            <h3 className="tips__title">Tips to Reduce Your Food Waste
-                </h3>
-                <div className="tips__container">
-                    <div className="tips__tip">
-                {wasteTips.slice(0, 3).map((tip) => (
-                    <div key={tip.id}>
-                        <p className="tips__tip"> <img className="tips__icon"src={lightbulb} alt="lightbulb"/>{tip.tip_text}</p>
-    
-                        </div>
-                    
-                   
-                ))}
-                   <button className="tips__btn">Show me More Tips!</button>
-                </div>
-              
-                
-                </div>
-               
+  
+  }, []);
+  const handleClick = () => {
+    getTips();
+}
+
+  return (
+    <div className="tips">
+      <h3 className="tips__title">Tips to Reduce Your Food Waste</h3>
+      <div className="tips__container">
+        <div className="tips__tip">
+          {wasteTips.slice(0, 3).map((tip) => (
+            <div key={tip.id}>
+              <p className="tips__tip">
+                {" "}
+                <img className="tips__icon" src={lightbulb} alt="lightbulb" />
+                {tip.tip_text}
+              </p>
+            </div>
+          ))}
+          <div className="tips__btn-container">
+          <button onClick={handleClick} className="tips__btn">Show me More Tips!</button>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default WasteTips;
--- waste-less-client/src/pages/ProfilePage/ProfilePage.js
+++ waste-less-client/src/pages/ProfilePage/ProfilePage.js
@@ -4,29 +4,31 @@ import C02Emissions from "../../components /C02Emissions/C02Emissions";
 import DisplayUserData from "../../components /DisplayUserData/DisplayUserData";
 import WasteTips from "../../components /WasteTips/WasteTips";
 import "./ProfilePage.scss";
-function ProfilePage () {
-    return (
-        <div className="profile">
-            <div className="profile__header">
-            <h1 className="profile__title"> Hello, User! Your Dashboard:</h1>
-            </div>
-            <DisplayUserData />
-            <div>
-            <div className="profile__data">
-            <CategoryGraph />
-            <div className="profile__waste-data">
-            <WasteOverTime />
-            </div>
-            </div>
-            <div className="profile__carbon-data">
-            <C02Emissions />
-            </div>
-            <div className="profile__waste-tips">
-            <WasteTips />
-            </div>
-            </div>
+function ProfilePage() {
+  return (
+    <div className="profile">
+      <div className="profile__header">
+        <h1 className="profile__title"> Hello, User! Your Dashboard:</h1>
+      </div>
+      <div className="profile__container">
+        <DisplayUserData />
+        <div className="profile__waste-tips">
+        <WasteTips />
         </div>
-    )
+      </div>
+      <div>
+        <div className="profile__data">
+          <CategoryGraph />
+          <div className="profile__waste-data">
+            <WasteOverTime />
+          </div>
+        </div>
+        <div className="profile__carbon-data">
+          <C02Emissions />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProfilePage;
